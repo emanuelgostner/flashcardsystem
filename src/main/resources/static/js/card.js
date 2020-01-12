a91050d7d3a3893358d3d93fac3336a5ce289c9a
Emanuel Gostner <emanuel.gostner@stud.fh-campuswien.ac.at>
2020-01-12 22:11:46 +0100

    (card) add card functionality

--- src/main/resources/static/js/card.js
+++ src/main/resources/static/js/card.js
@@ -1,5 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
+    /*
+    * Retrieve stackid from page-url
+    * */
     var stackid = /stacks\/(\d*)\//g.exec(window.location.href)[1];
+
+    /*
+    * Define common GUI elements
+    * */
     var view = {
         "cardsContainer": document.getElementsByClassName("cardsContainer")[0],
         "updateStack": {
@@ -7,41 +14,102 @@ document.addEventListener("DOMContentLoaded", function(event) {
             "button": document.getElementsByClassName("updateStackButton")[0]
         },
         "newCard": {
-            "question": document.getElementsByClassName("cardAnswer")[0],
-            "answer": document.getElementsByClassName("cardQuestion")[0]
+            "question": document.getElementsByClassName("cardQuestion")[0],
+            "answer": document.getElementsByClassName("cardAnswer")[0],
+            "button": document.getElementsByClassName("btnAddNewCard")[0]
+        }
+    };
+    /*
+    * Create Handlebars templates
+    * */
+    var newCardHtml = document.getElementById("newCard-template").innerHTML;
+    var HandlebarsNewCardTemplate = Handlebars.compile(newCardHtml);
+
+    /*
+    * Initialization
+    * */
+    init();
+    function init() {
+        initQuill(view.newCard.question);
+        initQuill(view.newCard.answer);
+        newCardFunctionality();
+        fetchAllCards();
+    }
+    function newCardFunctionality() {
+        view.newCard.button.addEventListener("click", function() {
+            var newCard = {
+            	"stackid":stackid,
+            	"question": view.newCard.question.querySelector(".ql-editor").innerHTML,
+            	"answer": view.newCard.answer.querySelector(".ql-editor").innerHTML
+            };
+            postNewCard(newCard);
+            var card = document.createElement("div");
+            card.innerHTML = HandlebarsNewCardTemplate([newCard]);
+            view.cardsContainer.appendChild(card);
+            initQuill(card.querySelector(".cardQuestion"));
+            initQuill(card.querySelector(".cardAnswer"));
+            view.newCard.question.querySelector(".ql-editor").innerHTML = "";
+            view.newCard.answer.querySelector(".ql-editor").innerHTML = "";
+        })
+
+    }
+    function fetchedCardsFunctionality(data) {
+        view.cardsContainer.innerHTML = HandlebarsNewCardTemplate(data);
+        var cardItems = document.getElementsByClassName("cardItem");
+        for(var i = 0; cardItems.length; i++) {
+            initQuill(cardItems[i].querySelector(".cardQuestion"));
+            initQuill(cardItems[i].querySelector(".cardAnswer"));
         }
     }
+    /*
+    * Create new Rich-text-editor instance
+    * */
+    function initQuill(domelement) {
+        new Quill(domelement, {
+            modules: {
+                toolbar: [
+                    [{header: [1, 2, false]}],
+                    ['bold', 'italic', 'underline'],
+                    [{'list': 'bullet'}],
+                ]
+            },
+            placeholder: 'Compose an epic...',
+            theme: 'snow'  // or 'bubble'
+        });
+    }
+    function fetchAllCards() {
+        fetch('/webapi/cards/getAllCards', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"stackid": stackid})
+        })
+            .then(res => res.json())
+            .then(function (data) {
+                console.log('Request succeeded with JSON response', data);
+                fetchedCardsFunctionality(data);
+            })
+            .catch(function (error) {
+                console.log('Request failed', error);
+            });
+    }
+    function postNewCard(newCard) {
+        fetch('/webapi/cards/addCard', {
+                   method: 'post',
+                   headers: {
+                       'Accept': 'application/json, text/plain, */*',
+                       'Content-Type': 'application/json'
+                   },
+                   body: JSON.stringify(newCard)
+               })
+                   .then(res => res.json())
+                   .then(function (response) {
 
-
-       // function postNewStack(newStackname) {
-       //     this.newStackname = newStackname;
-       //     fetch('./webapi/stacks/updateStack', {
-       //         method: 'post',
-       //         headers: {
-       //             'Accept': 'application/json, text/plain, */*',
-       //             'Content-Type': 'application/json'
-       //         },
-       //         body: JSON.stringify({
-       //                             "stackid": 2,
-       //                             "stackname": view.updateStack.input.value
-       //                            })
-       //     })
-       //         .then(res => res.json())
-       //         .then(function (data) {
-       //             var newCardData = {
-       //              "stackid": 2,
-       //              "stackname": view.updateStack.input.value
-       //             }
-       //             var my_elem = document.getElementsByClassName('addNewStack')[0];
-       //             var newCard = document.createElement('span');
-       //             newCard.innerHTML = HandlebarsCardTemplate(newCardData);
-       //             my_elem.parentNode.insertBefore(newCard, my_elem);
-       //             document.querySelector(".addNewStack input").value = "";
-       //             console.log('Request succeeded with JSON response', data);
-       //         })
-       //         .catch(function (error) {
-       //             console.log('Request failed', error);
-       //         });
-       // }
-
+                   })
+                   .catch(function (error) {
+                       console.log('Request failed', error);
+                   });
+    }
 })
